05c1d84118f418f294362a218bd5588876dd8e98
tooyangski <apungan.aubreyivansunga@gmail.com>
2024-10-27 17:44:15 +0800

    feat: add cors and fix packages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-// src/index.ts
 import express from "express";
 import productRoutes from "./routes/productRoute";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const cors = require("cors");
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/products", productRoutes);
